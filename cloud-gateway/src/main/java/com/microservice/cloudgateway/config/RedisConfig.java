3e0121a6196f90f56a7497258361d94dd1c82597
TayLQ <luongquoctay87@gmail.com>
2022-03-31 17:20:19 +0700

    update redis-client cofig connection

--- cloud-gateway/src/main/java/com/microservice/cloudgateway/config/RedisConfig.java
+++ cloud-gateway/src/main/java/com/microservice/cloudgateway/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.microservice.cloudgateway.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -19,10 +20,10 @@ public class RedisConfig {
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-        jedisConFactory.setHostName(redisHost);
-        jedisConFactory.setPort(redisPort);
-        return jedisConFactory;
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
