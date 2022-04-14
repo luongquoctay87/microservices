92e5fa3477589dfdb2bc69e98f03cdf9eb775c9f
thaoptbeetsoft <thaopt@beetsoft.com.vn>
2022-04-14 08:32:56 +0700

    format code

--- comment-service/src/main/java/com/microservice/historyservice/model/entity/FileLink.java
+++ comment-service/src/main/java/com/microservice/historyservice/model/entity/FileLink.java
@@ -6,15 +6,17 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Data
-@Entity(name="pa_file_link")
+@Entity(name = "pa_file_link")
 public class FileLink {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @Column(name = "comment_id")
     @NotNull(message = "không được để trống")
     private Long commentId;
-    @Column(name="link_file")
+
+    @Column(name = "link_file")
     @NotNull(message = "không được để trống")
     private String url;
 }
