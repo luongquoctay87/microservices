ddf80d75b4b69f305b663b85fd87333b9271ac05
thaoptbeetsoft <thaopt@beetsoft.com.vn>
2022-04-21 11:55:42 +0700

    docker run comment

--- auth-server/src/main/java/com/microservice/authserver/config/WebSecurityConfig.java
+++ auth-server/src/main/java/com/microservice/authserver/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.microservice.authserver.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
